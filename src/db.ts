beb7b378862f5939e9b49e5581266815a8386b80
Nicole Silvestrini Garrio <nsgarrio@gmail.com>
2024-10-31 17:52:01 -0300

    Redis funcionando

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import mysql, { ConnectionOptions } from 'mysql2';
 import dotenv from 'dotenv';
-//import { createClient } from 'redis';
+import { createClient } from 'redis';
 dotenv.config();
 
 const access: ConnectionOptions = {
@@ -21,15 +21,21 @@ conn.connect((err) => {
 });
 
 // Conexão com Redis
-// const redisClient = createClient({
-//     url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
-// });
+const redisClient = createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+});
 
-// (async () => {
-//     try {
-//         await redisClient.connect();
-//         console.log('Conectado ao Redis');
-//     } catch (err) {
-//         console.error('Erro ao conectar ao Redis:', err);
-//     }
-// })();
+(async () => {
+    try {
+        await redisClient.connect();
+        console.log('Conectado ao Redis');
+    } catch (err) {
+        console.error('Erro ao conectar ao Redis:', err);
+    }
+})();
+
+// Lidar com erros de conexão do Redis
+redisClient.on('error', (err) => console.error('Redis Client Error:', err));
+
+// Exportar redisClient para usar em outras partes da aplicação
+export { redisClient };
